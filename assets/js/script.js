d2b7e5df2942c3a6dba9d2e1251b7116ee579000
Wilson Buenaobra <wvbuenaobra@gmail.com>
2018-11-27 16:42:06 +0800

    Color Palette

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+document.getElementById('sel-red').onclick = function makeRed() {
+	document.getElementById('box-red').style.backgroundColor = 'red';
+}
+
+document.getElementById('sel-orange').onclick = function makeOrange() {
+	document.getElementById('box-orange').style.backgroundColor = 'darkorange';
+}
+
+document.getElementById('sel-yellow').onclick = function makeYellow() {
+	document.getElementById('box-yellow').style.backgroundColor = 'yellow';
+}
+
+document.getElementById('sel-green').onclick = function makeGreen() {
+	document.getElementById('box-green').style.backgroundColor = 'green';
+}
+
+document.getElementById('sel-blue').onclick = function makeBlue() {
+	document.getElementById('box-blue').style.backgroundColor = 'blue';
+}
+
+document.getElementById('sel-violet').onclick = function makeViolet() {
+	document.getElementById('box-violet').style.backgroundColor = 'darkviolet';
+}
+
+document.getElementById('sel-pink').onclick = function makePink() {
+	document.getElementById('box-red').style.backgroundColor = 'pink';
+	document.getElementById('box-orange').style.backgroundColor = 'pink';
+	document.getElementById('box-yellow').style.backgroundColor = 'pink';
+	document.getElementById('box-green').style.backgroundColor = 'pink';
+	document.getElementById('box-blue').style.backgroundColor = 'pink';
+	document.getElementById('box-violet').style.backgroundColor = 'pink';
+}
+
+document.getElementById('sel-white').onclick = function makeWhite() {
+	document.getElementById('box-red').style.backgroundColor = 'white';
+	document.getElementById('box-orange').style.backgroundColor = 'white';
+	document.getElementById('box-yellow').style.backgroundColor = 'white';
+	document.getElementById('box-green').style.backgroundColor = 'white';
+	document.getElementById('box-blue').style.backgroundColor = 'white';
+	document.getElementById('box-violet').style.backgroundColor = 'white';
+}
